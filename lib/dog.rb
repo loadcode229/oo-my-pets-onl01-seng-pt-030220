f08a2c87cc502e7363b1f372420487a42b0bf1f3
Adam Priest <sepone229@gmail.com>
2020-03-31 01:12:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,8 +3,14 @@ class Dog
   attr_accessor :mood, :owner
   attr_reader :name
   
+  @@dogs = []
+  
   def initialize(name, owner, mood = 'nervous')
     @name, @owner, @mood = name, owner, mood
+    @@dogs << self
   end
   
+  def self.all
+    @@dogs
+  end
 end
