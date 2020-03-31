e040f6ff58164789aaced9e3a6b4dedff9394c5d
Adam Priest <sepone229@gmail.com>
2020-03-31 01:12:18 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -4,8 +4,10 @@ class Cat
   attr_accessor :mood, :owner
   attr_reader :name
   @@pets = []
+  
   def initialize(name, owner, mood = "nervous")
     @name, @owner, @mood = name, owner, mood
+    
   end
   def self.all
     @@pets
